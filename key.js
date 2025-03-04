5c0cfea4deb21a4ff8ab19520e5123c8c5d7638e
vineela26-create <23pa1a12l2@vishnu.edu.in>
2025-03-04 22:39:08 +0530

    Create key.js

---
+++ key.js
@@ -0,0 +1,13 @@
+{
+  "type": "service_account",
+  "project_id": "project6-ff61b",
+  "private_key_id": "0ea8eb194084b9bfa2d4dd2cafd4d143fc9bb0df",
+  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDadDX4/uQgojJP\nXTjbcRE9Av5YlDicuI0cpMcmq+iG+6ZJooNNgyrXcuMVPolnjC3ZVcJ3C0yqEQfa\nKDrVm1d5xHQ43sCRwygwpPyRTXa9MYK4dp285guIiqgC63VC/L6EcG0Zx9lOCplk\nrLzctmspK7u+f5nZl2RLXn03s4xZgDfFqAlKWNlxRDt7jEYzn+m16TjphVrpqQpZ\nJIYM9lrEg0F6wyvWLFKn6xODKmpQyqjSqMex3kg9kUaUpWl0GBZZ87o5Mm3ldXqq\nNKoXq3eL3e+8BschJDXuDCwdI5Nu4y9morIINIzEU4W2zCGVPGc9Uwj99y2xP8Go\n+j08Oq5pAgMBAAECggEABZBER/Xij06KMPlrEghTESZ4c1Q9DTK/DJ3JYPurqvUR\nmFpTn+pUBujVuIwJ7vxD4P8yj2/B0VVTNElmLDFYsjt1s/VqWpv+3p7zvBXzYktL\n7LuVS1hhg/tMtEb/tXJ0k1topT7bfYW+v9tis6KlX9xNsQLqGGpRuICszY8cpzJ/\nzOqVewt5MWhtArM/LLJpYUr3GgRlvXxRifoA5PMBJ56w7rKsdy8RYLF7qA66VRBH\nnlJDh4MotmwbR7625jAYKg8YEl/VON2/MoO2xw5SejgOtjuttvnTsiIIuTOcLcC6\nxwLZbfgKPLgf6OfLKiPjkmZ34FAxu0pIkSNbb/xWAQKBgQDw8yZXCOQTzeRi7arP\n8jJ7g+QZsydQWiGClX/JlncUX1WYfa4L1tNRIyWWtzDj3tm5PaelZ2ZwhiVsx9U/\nhzZjry56UpgPpfBwWXKSdJPMtWaAneolKnbjB18ENumzzGReErVFXbFFvLuX8a/f\nAfVIl2TXMWZxXY/8EEzU5cn9aQKBgQDoGViyRjfH3hmMLaxpmQf8I/vvee4p+EpD\n1iuMoQylyjA4nkDZ0s/Z8gx8a34MK9SVjzg+tn5nQ/SoeTxmk1kzpZsDKwlsn08K\nqKg6ne0c2tumv8afE6v7Iwvc7OkmwlKQSM0IVXg3J1aYgAvm3ZtKn4iXMItCICzL\n6OFDX0gJAQKBgQDgHHkTyQxDM2VzcrZG9nJoGjPmh5FQlpTGngEe4pnd1BKMZJGn\nJ2ij9jn3JmsN9/Cw8JznJvbMQKm8tV4u2bgIHh/v3LTdES5DgZ8kg9QZOrydcsQf\nT5EG94s0I1NaTgkoyuPaIhxJzhL6b+1yUqLvowlDcwpi9XCANjUQbK9kyQKBgD2A\naeZjgOhvS+m/OI6KxHjluB6O+rMYXeV33PZYvpc36ms2VAzkNbuUzgYyHaZv/tba\nqpbDmN8YPL5f0ceEiACNg3PldYvY8wLqChnuOySIQef/rUv91HappL0X8fgZSfN9\ntwbcTwyeWe3zEDROyJWPjIwzitMs33Hw9a/6kYYBAoGANlIYlzoZs+BEYeS6BbBy\nVNmxqFBaWWV7O9kbkZaYDPTQ02WVWk3H9iQDNOr5GydkwY9CHdtO61Pvnm2aqn5y\n7TczwdwnJfdCoycR4R/z1g4GyWnLDNxffCnup8GTyTCsbCGkn64oYYftfD4qmbRE\nFsqHrxgr+31JewdwHZ0swBY=\n-----END PRIVATE KEY-----\n",
+  "client_email": "firebase-adminsdk-fbsvc@project6-ff61b.iam.gserviceaccount.com",
+  "client_id": "110631506228098677725",
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-fbsvc%40project6-ff61b.iam.gserviceaccount.com",
+  "universe_domain": "googleapis.com"
+}
